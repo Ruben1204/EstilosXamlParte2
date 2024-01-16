21ea0fddf52333625d9762b1e5210baa768db3e1
Ruben Darío Rueda <ingeniero.rubenrueda@gmail.com>
2024-01-16 17:15:17 -0500

    FirstCommit
    Add excercise with Resources Dynamic and class

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EstilosXamlSecondPart
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace EstilosXamlSecondPart
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Resources["blueSearchBar"] = Resources["greenSearchBar"];
+        }
+    }
+
+}
